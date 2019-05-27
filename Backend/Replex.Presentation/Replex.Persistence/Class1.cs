8a3972786a3f9cbb4677d6ff580451e06bd974a1
ace <adamah.chiniah-baulajee@elca.mu>
2019-05-27 16:50:11 +0400

    Set up architecture

---
+++ Backend/Replex.Presentation/Replex.Persistence/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Replex.Persistence
+{
+  public class Class1
+  {
+  }
+}
---
+++ Backend/Replex.Presentation/Replex.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Replex.Services
+{
+  public class Class1
+  {
+  }
+}
