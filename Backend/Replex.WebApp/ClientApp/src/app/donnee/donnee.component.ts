31487e7997102071c697350c574715c2e4439e8e
Yogeshwaree Ahotar <yogeshwaree.ahotar@elca.ch>
2019-06-04 10:58:23 +0400

    Add new DonneeComponent

---
+++ Backend/Replex.WebApp/ClientApp/src/app/donnee/donnee.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-donnee',
+  templateUrl: './donnee.component.html',
+  styleUrls: ['./donnee.component.css']
+})
+export class DonneeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
